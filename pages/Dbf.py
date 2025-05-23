ba436e547e33ca79056bdd6163eea05a279331ff
ControladoriaF <custos_orcamento.ferreira@outlook.com>
2025-05-23 13:38:28 -0300

    Add files via upload

---
+++ pages/Dbf.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+from dbfread import DBF
+import io
+from io import BytesIO
+
+st.title("Download Arquivo .DBF ")
+
+uploaded_file = st.file_uploader("Importe", type=["dbf"])
+
+
+#streamlit run Importação_de_arquivo.py
+
+if uploaded_file is not None:
+    # Read the uploaded .dbf file using dbfread
+    with open("temp_file.dbf", "wb") as f:
+        f.write(uploaded_file.read())
+
+    table = DBF("temp_file.dbf", load=True, encoding='latin-1')
+    df = pd.DataFrame(iter(table))
+
+    modified_excel = BytesIO()
+
+    with pd.ExcelWriter(modified_excel, engine="xlsxwriter") as writer:
+                        df.to_excel(writer, index=False, sheet_name="Tranferências")
+            
+    modified_excel.seek(0)
+
+    download_geral = st.download_button(
+                    label="Debito",
+                    data=modified_excel,
+                    file_name=F"Banco.xlsx",
+                    mime="text/csv"
+                )
+
+
+
+    st.success("Arquivo Carregado")
+    st.dataframe(df)
